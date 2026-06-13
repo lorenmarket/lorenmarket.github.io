f2bd001e280dd59ab4b2fd7227da8621f8ccdb1f
halangdon <27764036+halangdon@users.noreply.github.com>
2026-06-14 00:41:26 +0800

    Set up eleventy-base-blog with GitHub Pages deploy

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Loren Market",
+	url: "https://lorenmarket.github.io/",
+	language: "en",
+	description: "Loren Market",
+	author: {
+		name: "Loren Market",
+		email: "junlee.pepito@gmail.com",
+		url: "https://lorenmarket.github.io/about/"
+	}
+}
